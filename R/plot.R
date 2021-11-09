f246315d2b437a687365108989ae8ae2973c5ab3
jrboyd <jrboyd@med.uvm.edu>
2021-11-09 10:08:55 -0500

    docs and color mapping for line plots

--- R/plot.R
+++ R/plot.R
@@ -1,22 +1,22 @@
 #' load_counts
 #'
-#' @param psc
+#' @param psc peaksat_config object
 #'
-#' @return
+#' @return data.table of peak count vs read count.
 #' @export
 #'
 #' @examples
 load_counts = function(psc){
- wds = dir(get_result_dir(psc), full.names = TRUE)
- wds = wds[basename(wds) != "sub_logs"]
- load_counts.wd(wds)
+  wds = dir(get_result_dir(psc), full.names = TRUE)
+  wds = wds[basename(wds) != "sub_logs"]
+  load_counts.wd(wds)
 }
 
-#' Title
+#' load_counts.wd
 #'
-#' @param wds
+#' @param wds directories to load count data from.
 #'
-#' @return
+#' @return data.table of peak count vs read count.
 #' @export
 #'
 #' @examples
@@ -80,60 +80,95 @@ load_counts.wd = function(wds){
   cnt_dt
 }
 
-#' Title
+#' plot_peak_saturation_lines
 #'
-#' @param cnt_dt
+#' @param cnt_dt data.table returned by load_counts()
+#' @param color_by variable in cnt_dt to map to color.
 #'
-#' @return
+#' @return ggplot of peak count vs read count.
 #' @export
 #' @import ggplot2
 #' @examples
-plot_peak_saturation_lines = function(cnt_dt){
+plot_peak_saturation_lines = function(cnt_dt, color_by = NULL){
   lab_dt = data.table::rbindlist(lapply(split(cnt_dt, cnt_dt$sample), function(x){
     x[x$read_count == max(x$read_count),]
   }))
   lab_dt$read_count_lab = lab_dt$read_count + 8e5
-  p_lines = ggplot(cnt_dt, aes(x = read_count, y = peak_count, group = sample)) +
-    geom_path() +
-    geom_point(data = lab_dt) +
-    geom_text(data = lab_dt, aes(label = sample, x = read_count_lab), hjust = 0) +
-    scale_x_continuous(expand = expansion(c(.1, .5)), labels = function(x)x/1e6) +
-    scale_y_continuous(labels = function(x)x/1e3) +
-    labs(x = "reads (M)", y = "peaks (k)") +
-    cowplot::theme_cowplot() +
-    expand_limits(y = 0)
+  if(is.null(color_by)){
+    p_lines = ggplot(cnt_dt, aes(x = read_count, y = peak_count, group = sample)) +
+      geom_path() +
+      geom_point(data = lab_dt) +
+      geom_text(data = lab_dt, aes(label = sample, x = read_count_lab), hjust = 0) +
+      scale_x_continuous(expand = expansion(c(.1, .5)), labels = function(x)x/1e6) +
+      scale_y_continuous(labels = function(x)x/1e3) +
+      labs(x = "reads (M)", y = "peaks (k)") +
+      cowplot::theme_cowplot() +
+      expand_limits(y = 0)
+  }else{
+    if(!color_by %in% colnames(cnt_dt)) stop(color_by, " not in colnames of cnt_dt.")
+    p_lines = ggplot(cnt_dt, aes_string(x = "read_count", y = "peak_count", group = "sample", color = color_by)) +
+      geom_path() +
+      geom_point(data = lab_dt) +
+      geom_text(data = lab_dt, aes(label = sample, x = read_count_lab), hjust = 0) +
+      scale_x_continuous(expand = expansion(c(.1, .5)), labels = function(x)x/1e6) +
+      scale_y_continuous(labels = function(x)x/1e3) +
+      labs(x = "reads (M)", y = "peaks (k)") +
+      cowplot::theme_cowplot() +
+      expand_limits(y = 0)
+  }
   p_lines
 }
 
-#' Title
+#' plot_peak_saturation_lines.facetted
 #'
-#' @param cnt_dt
+#' @param cnt_dt data.table returned by load_counts()
+#' @param color_by variable in cnt_dt to map to color.
 #'
-#' @return
+#' @return ggplot of peak count vs read count. facetted by sample.
 #' @export
 #' @import ggplot2
 #' @examples
-plot_peak_saturation_lines.facetted = function(cnt_dt){
+plot_peak_saturation_lines.facetted = function(cnt_dt, color_by = NULL){
   lab_dt = data.table::rbindlist(lapply(split(cnt_dt, cnt_dt$sample), function(x){
     x[x$read_count == max(x$read_count),]
   }))
   lab_dt$read_count_lab = lab_dt$read_count + 8e5
-  p = ggplot(cnt_dt, aes(x = read_count, y = peak_count, group = sample)) +
-    cowplot::theme_cowplot() + theme(strip.text = element_text(size = 8)) +
-    scale_x_continuous(expand = expansion(c(.1, .5)), labels = function(x)x/1e6) +
-    scale_y_continuous(labels = function(x)x/1e3) +
-    labs(x = "reads (M)", y = "peaks (k)")
-  cnt_dtl = split(cnt_dt, cnt_dt$sample)
-  for(dt in cnt_dtl){
-    p = p + annotate("path", x = dt$read_count, y = dt$peak_count, color = "gray80")
-  }
+  if(is.null(color_by)){
+    p = ggplot(cnt_dt, aes(x = read_count, y = peak_count, group = sample)) +
+      cowplot::theme_cowplot() + theme(strip.text = element_text(size = 8)) +
+      scale_x_continuous(expand = expansion(c(.1, .5)), labels = function(x)x/1e6) +
+      scale_y_continuous(labels = function(x)x/1e3) +
+      labs(x = "reads (M)", y = "peaks (k)")
+    cnt_dtl = split(cnt_dt, cnt_dt$sample)
+    for(dt in cnt_dtl){
+      p = p + annotate("path", x = dt$read_count, y = dt$peak_count, color = "gray80")
+    }
 
-  p = p +
-    geom_path() +
-    geom_point(data = lab_dt) +
-    facet_wrap(~sample) +
-    expand_limits(y = 0)
-  p_lines2 = p
+    p = p +
+      geom_path() +
+      geom_point(data = lab_dt) +
+      facet_wrap(~sample) +
+      expand_limits(y = 0)
+    p_lines2 = p
+  }else{
+    if(!color_by %in% colnames(cnt_dt)) stop(color_by, " not in colnames of cnt_dt.")
+    p = ggplot(cnt_dt, aes_string(x = "read_count", y = "peak_count", group = "sample", color = color_by)) +
+      cowplot::theme_cowplot() + theme(strip.text = element_text(size = 8)) +
+      scale_x_continuous(expand = expansion(c(.1, .5)), labels = function(x)x/1e6) +
+      scale_y_continuous(labels = function(x)x/1e3) +
+      labs(x = "reads (M)", y = "peaks (k)")
+    cnt_dtl = split(cnt_dt, cnt_dt$sample)
+    for(dt in cnt_dtl){
+      p = p + annotate("path", x = dt$read_count, y = dt$peak_count, color = "gray80")
+    }
+
+    p = p +
+      geom_path() +
+      geom_point(data = lab_dt) +
+      facet_wrap(~sample) +
+      expand_limits(y = 0)
+    p_lines2 = p
+  }
   p_lines2
 }
 
