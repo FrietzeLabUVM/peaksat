634074eb75bc04fefed448742c1df0d6727fa992
jrboyd <jrboyd@med.uvm.edu>
2021-11-09 10:46:06 -0500

    bugfix for group

--- R/plot.R
+++ R/plot.R
@@ -90,12 +90,12 @@ load_counts.wd = function(wds){
 #' @import ggplot2
 #' @examples
 plot_peak_saturation_lines = function(cnt_dt, color_by = NULL){
+  cnt_dt$group = paste(cnt_dt$out_dir, cnt_dt$sample, cnt_dt$peak_stat)
   lab_dt = data.table::rbindlist(lapply(split(cnt_dt, cnt_dt$sample), function(x){
     x[x$read_count == max(x$read_count),]
   }))
   lab_dt$read_count_lab = lab_dt$read_count + 8e5
   if(is.null(color_by)){
-    cnt_dt$group = paste(cnt_dt$out_dir, cnt_dt$sample, cnt_dt$peak_stat)
     p_lines = ggplot(cnt_dt, aes(x = read_count, y = peak_count, group = group)) +
       geom_path() +
       geom_point(data = lab_dt) +
@@ -107,7 +107,6 @@ plot_peak_saturation_lines = function(cnt_dt, color_by = NULL){
       expand_limits(y = 0)
   }else{
     if(!color_by %in% colnames(cnt_dt)) stop(color_by, " not in colnames of cnt_dt.")
-    cnt_dt$group = paste(cnt_dt$out_dir, cnt_dt$sample, cnt_dt$peak_stat)
     p_lines = ggplot(cnt_dt, aes_string(x = "read_count", y = "peak_count", group = "group", color = color_by)) +
       geom_path() +
       geom_point(data = lab_dt) +
@@ -131,12 +130,12 @@ plot_peak_saturation_lines = function(cnt_dt, color_by = NULL){
 #' @import ggplot2
 #' @examples
 plot_peak_saturation_lines.facetted = function(cnt_dt, color_by = NULL){
+  cnt_dt$group = paste(cnt_dt$out_dir, cnt_dt$sample, cnt_dt$peak_stat)
   lab_dt = data.table::rbindlist(lapply(split(cnt_dt, cnt_dt$sample), function(x){
     x[x$read_count == max(x$read_count),]
   }))
   lab_dt$read_count_lab = lab_dt$read_count + 8e5
   if(is.null(color_by)){
-    cnt_dt$group = paste(cnt_dt$out_dir, cnt_dt$sample, cnt_dt$peak_stat)
     p = ggplot(cnt_dt, aes(x = read_count, y = peak_count, group = group)) +
       cowplot::theme_cowplot() + theme(strip.text = element_text(size = 8)) +
       scale_x_continuous(expand = expansion(c(.1, .5)), labels = function(x)x/1e6) +
@@ -155,7 +154,6 @@ plot_peak_saturation_lines.facetted = function(cnt_dt, color_by = NULL){
     p_lines2 = p
   }else{
     if(!color_by %in% colnames(cnt_dt)) stop(color_by, " not in colnames of cnt_dt.")
-    cnt_dt$group = paste(cnt_dt$out_dir, cnt_dt$sample, cnt_dt$peak_stat)
     p = ggplot(cnt_dt, aes_string(x = "read_count", y = "peak_count", group = "group", color = color_by)) +
       cowplot::theme_cowplot() + theme(strip.text = element_text(size = 8)) +
       scale_x_continuous(expand = expansion(c(.1, .5)), labels = function(x)x/1e6) +
