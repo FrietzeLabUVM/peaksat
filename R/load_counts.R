8ab92fb53bc5ad6bc1ef1368e19ef70688ccf967
jrboyd <jrboyd@med.uvm.edu>
2022-02-04 14:25:57 -0500

    big rework of load_counts added load_widths()

--- R/load_counts.R
+++ R/load_counts.R
@@ -1,101 +1,111 @@
 #' load_counts
 #'
 #' @param psc peaksat_config object
+#' @param min_signalValue
 #'
 #' @return data.table of peak count vs read count.
 #' @export
 #'
 #' @examples
 load_counts = function(psc, min_signalValue = 1){
+  .general_load_info(psc, min_signalValue = min_signalValue, type = "count")
+}
+
+#' load_widths
+#'
+#' @param psc peaksat_config object
+#' @param min_signalValue
+#'
+#' @return data.table of peak count vs read count.
+#' @export
+#'
+#' @examples
+load_widths = function(psc, min_signalValue = 1){
+  .general_load_info(psc, min_signalValue = min_signalValue, type = "width")
+}
+
+.general_load_info = function(psc, min_signalValue = 1, type = c("count", "width")[1]){
   wds = dir(get_result_dir(psc), full.names = TRUE)
   wds = wds[basename(wds) != "sub_logs"]
-  load_counts.wd(wds, min_signalValue = min_signalValue)
+  .general_load_info.wd(wds, min_signalValue = min_signalValue, type = type)
 }
 
-.load_peak_counts = function(peak_count_file, min_signalValue = min_signalValue){
-  cn = c("name", "seed", "fraction", "stat", "cutoff", "PE", "input")
-
-  count_files = sapply(min_signalValue, function(min_sig){
-    odir = file.path(dirname(peak_count_file), paste0("counts_signalValue_", min_sig))
-    dir.create(odir, showWarnings = FALSE)
-    ofile = file.path(odir, basename(peak_count_file))
-    ofile
-  })
-  peak_file = sub(".bam.peak_count", "_peaks.narrowPeak", peak_count_file)
-  if(any(!file.exists(count_files))){
-    message("Loading peak file for counting: ", peak_file)
-    if(!file.exists(peak_file)) stop("couldn't find peak file: ", peak_file)
-    .peak_gr = seqsetvis::easyLoad_narrowPeak(peak_file)[[1]]
-  }else{
-    message("Using saved peak counts for: ", peak_file)
-  }
-  count_res = list()
-  for(i in seq_along(min_signalValue)){
-    min_sig = min_signalValue[i]
-    odir = file.path(dirname(peak_count_file), paste0("counts_signalValue_", min_sig))
-    dir.create(odir, showWarnings = FALSE)
-    ofile = file.path(odir, basename(peak_count_file))
-    if(file.exists(ofile)){
-      dt = data.table::fread(ofile, col.names = c("peak_count", cn))
-      dt$signal_cutoff = min_sig
-      dt = dt[, c("signal_cutoff", "peak_count", cn), with = FALSE]
-    }else{
-      n_peaks = length(subset(.peak_gr, signalValue >= min_sig))
-      tmp_dt = fread(peak_count_file, col.names = c("peak_count", cn))
-      out_dt = copy(tmp_dt)
-      out_dt$peak_count = n_peaks
-      fwrite(out_dt, ofile, sep = " ")
-      out_dt$signal_cutoff = min_sig
-      dt = out_dt[, c("signal_cutoff", "peak_count", cn), with = FALSE]
+#' .general_load_info.wd
+#'
+#' @param wds directories to load count data from.
+#'
+#' @return data.table of peak count vs read count.
+#'
+#' @examples
+.general_load_info.wd = function(wds, min_signalValue = 1, type = c("count", "width")[1]){
+  wds = wds[basename(wds) != "sub_logs"]
+  keep = sapply(wds, function(wd){
+    read_count_files = dir(wd, pattern = "read_count$", full.names = TRUE)
+    np_files = dir(wd, pattern = "narrowPeak$", full.names = TRUE)
+    if(length(read_count_files) == 0 || length(np_files) == 0){
+      return(FALSE)
     }
-    count_res[[i]] = dt
+    if(length(read_count_files) != length(np_files)){
+      return(FALSE)
+    }
+    return(TRUE)
+  })
+  if(any(!keep)){
+    warning("Not all results appear to be complete.")
   }
-  out_dt = rbindlist(count_res)
+  if(!any(keep)) stop ("No valid results found.")
 
-  # for(min_sig in min_signalValue){
-  #   odir = file.path(dirname(peak_count_file), paste0("counts_signalValue_", min_sig))
-  #   dir.create(odir, showWarnings = FALSE)
-  #   ofile = file.path(odir, basename(peak_count_file))
-  #   if(file.exists(ofile)){
-  #       dt = data.table::fread(ofile, col.names = c("peak_count", cn))
-  #       dt$signal_cutoff = min_sig
-  #       dt = dt[, c("signal_cutoff", "peak_count", cn), with = FALSE]
-  #   }else{
-  #     peak_file = sub(".bam.peak_count", "_peaks.narrowPeak", peak_count_file)
-  #     if(!file.exists(peak_file)) stop("couldn't find peak file: ", peak_file)
-  #     peak_gr = seqsetvis::easyLoad_narrowPeak(peak_file)[[1]]
-  #     n_peaks = length(subset(peak_gr, signalValue >= min_sig))
-  #     tmp_dt = fread(peak_count_file, col.names = c("peak_count", cn))
-  #     out_dt = copy(tmp_dt)
-  #     out_dt$peak_count = n_peaks
-  #     fwrite(out_dt, ofile, sep = " ")
-  #     out_dt$signal_cutoff = min_sig
-  #     dt = out_dt[, c("signal_cutoff", "peak_count", cn), with = FALSE]
-  #   }
-  # }
-  # if(file.exists(sub(".peak_count", ".peak_cutoff_counts", peak_count_file))){
-  #   peak_cutoff_file = sub(".peak_count", ".peak_cutoff_counts", peak_count_file)
-  #   dt = data.table::fread(peak_cutoff_file, col.names = c("signal_cutoff", "peak_count", cn))
-  # }else{
-  #   dt = data.table::fread(peak_count_file, col.names = c("peak_count", cn))
-  #   dt$signal_cutoff = 1
-  #   dt = dt[, c("signal_cutoff", "peak_count", cn), with = FALSE]
-  # }
-  out_dt[]
+
+  peak_FUN = switch(type,
+                    count = {
+                      .load_peak_counts
+                    },
+                    width = {
+                      .load_peak_widths
+                    }, {
+                      NULL
+                    })
+  if(is.null(peak_FUN)){
+    stop("Unrecognized type for peak_FUN: ", type)
+  }
+
+  if(.Platform$OS.type == "windows" || getOption("mc.cores", 1) == 1) {
+    cnt_dtl = pbapply::pblapply(wds[keep], .assemble_peak_info, min_signalValue = min_signalValue, info_name = type, peak_FUN = peak_FUN)
+
+  } else {
+    cnt_dtl = pbmcapply::pbmclapply(wds[keep], .assemble_peak_info, min_signalValue = min_signalValue, info_name = type, peak_FUN = peak_FUN)
+  }
+  cnt_dt = data.table::rbindlist(cnt_dtl)
+
+  cnt_dt[]
 }
 
-.load_counts = function(wd, min_signalValue = 1){
+.get_template = function(np_file){
+  suffix = "count"
+  odir = dirname(np_file)
+  ofile = file.path(odir, sub("_peaks.narrowPeak", paste0(".bam.peak_", suffix), basename(np_file)))
+  ofile
+}
+
+.get_ofile = function(np_file, min_sig, suffix = c("count", "width")[1]){
+  odir = file.path(dirname(np_file), paste0("counts_signalValue_", min_sig))
+  dir.create(odir, showWarnings = FALSE)
+  ofile = file.path(odir, sub("_peaks.narrowPeak", paste0(".bam.peak_", suffix), basename(np_file)))
+  ofile
+}
+
+.assemble_peak_info = function(wd, min_signalValue = 1, info_name = c("count", "width")[1], peak_FUN = .load_peak_counts){
   read_count_files = dir(wd, pattern = "read_count$", full.names = TRUE)
-  peak_count_files = dir(wd, pattern = "peak_count$", full.names = TRUE)
+  np_files = dir(wd, pattern = "narrowPeak$", full.names = TRUE)
 
   names(read_count_files) = sub(".bam.+", "", basename(read_count_files))
-  names(peak_count_files) = sub(".bam.+", "", basename(peak_count_files))
+  names(np_files) = sub("_peaks.narrowPeak", "", basename(np_files))
 
   cn = c("name", "seed", "fraction", "stat", "cutoff", "PE", "input")
   rc_dt =  data.table::rbindlist(lapply(read_count_files, data.table::fread, col.names = c("read_count", cn)), idcol = "wd")
-  pc_dt =  data.table::rbindlist(lapply(peak_count_files, .load_peak_counts, min_signalValue = min_signalValue), idcol = "wd")
+  pc_dt =  data.table::rbindlist(lapply(np_files, peak_FUN, min_signalValue = min_signalValue), idcol = "wd")
 
-  cnt_dt = merge(rc_dt, pc_dt[, c("name", "peak_count", "signal_cutoff")], by = "name")
+  cnt_dt = merge(rc_dt, pc_dt[, c("name", paste0("peak_", info_name), "signal_cutoff"), with = FALSE], by = "name")
 
   data.table::set(cnt_dt, j = c("fraction", "rep"), value = data.table::tstrsplit(cnt_dt$name, "\\.", keep = 2:3))
   cnt_dt$fraction = as.numeric(cnt_dt$fraction)/100
@@ -121,39 +131,78 @@ load_counts = function(psc, min_signalValue = 1){
   cnt_dt[order(cnt_dt$read_count),]
 }
 
-#' load_counts.wd
-#'
-#' @param wds directories to load count data from.
-#'
-#' @return data.table of peak count vs read count.
-#' @export
-#'
-#' @examples
-load_counts.wd = function(wds, min_signalValue = 1){
-  wds = wds[basename(wds) != "sub_logs"]
-  keep = sapply(wds, function(wd){
-    read_count_files = dir(wd, pattern = "read_count$", full.names = TRUE)
-    peak_count_files = dir(wd, pattern = "peak_count$", full.names = TRUE)
-    if(length(read_count_files) == 0 || length(peak_count_files) == 0){
-      return(FALSE)
-    }
-    if(length(read_count_files) != length(peak_count_files)){
-      return(FALSE)
-    }
-    return(TRUE)
+
+
+#loads a vector of peak_count_files
+.load_peak_counts = function(np_file, min_signalValue = min_signalValue){
+  cn = c("name", "seed", "fraction", "stat", "cutoff", "PE", "input")
+  count_files = sapply(min_signalValue, function(min_sig){
+    .get_ofile(np_file, min_sig, "count")
   })
-  if(any(!keep)){
-    warning("Not all results appear to be complete.")
+  if(any(!file.exists(count_files))){
+    message("Loading peak file for counting: ", np_file)
+    if(!file.exists(np_file)) stop("couldn't find peak file: ", np_file)
+    .peak_gr = seqsetvis::easyLoad_narrowPeak(np_file)[[1]]
+  }else{
+    message("Using saved peak counts for: ", np_file)
   }
-  if(!any(keep)) stop ("No valid results found.")
-
-  if(.Platform$OS.type == "windows" || getOption("mc.cores", 1) == 1) {
-    cnt_dtl = pbapply::pblapply(wds[keep], .load_counts, min_signalValue = min_signalValue)
-
-  } else {
-    cnt_dtl = pbmcapply::pbmclapply(wds[keep], .load_counts, min_signalValue = min_signalValue)
+  count_res = list()
+  for(i in seq_along(min_signalValue)){
+    min_sig = min_signalValue[i]
+    ofile = .get_ofile(np_file, min_sig, "count")
+    if(file.exists(ofile)){
+      dt = data.table::fread(ofile, col.names = c("peak_count", cn))
+      dt$signal_cutoff = min_sig
+      dt = dt[, c("signal_cutoff", "peak_count", cn), with = FALSE]
+    }else{
+      n_peaks = length(subset(.peak_gr, signalValue >= min_sig))
+      tmp_dt = fread(.get_template(np_file), col.names = c("peak_count", cn))
+      out_dt = copy(tmp_dt)
+      out_dt$peak_count = n_peaks
+      fwrite(out_dt, ofile, sep = " ")
+      out_dt$signal_cutoff = min_sig
+      dt = out_dt[, c("signal_cutoff", "peak_count", cn), with = FALSE]
+    }
+    count_res[[i]] = dt
   }
-  cnt_dt = data.table::rbindlist(cnt_dtl)
+  out_dt = rbindlist(count_res)
 
-  cnt_dt[]
+  out_dt[]
+}
+
+#loads a vector of peak_width_files
+.load_peak_widths = function(np_file, min_signalValue = min_signalValue){
+  cn = c("name", "seed", "fraction", "stat", "cutoff", "PE", "input")
+  width_files = sapply(min_signalValue, function(min_sig){
+    .get_ofile(np_file, min_sig, "width")
+  })
+  if(any(!file.exists(width_files))){
+    message("Loading peak file for totaling width: ", np_file)
+    if(!file.exists(np_file)) stop("couldn't find peak file: ", np_file)
+    .peak_gr = seqsetvis::easyLoad_narrowPeak(np_file)[[1]]
+  }else{
+    message("Using saved peak widths for: ", np_file)
+  }
+  width_res = list()
+  for(i in seq_along(min_signalValue)){
+    min_sig = min_signalValue[i]
+    ofile = .get_ofile(np_file, min_sig, "width")
+    if(file.exists(ofile)){
+      dt = data.table::fread(ofile, col.names = c("peak_width", cn))
+      dt$signal_cutoff = min_sig
+      dt = dt[, c("signal_cutoff", "peak_width", cn), with = FALSE]
+    }else{
+      peak_width = sum(width(subset(.peak_gr, signalValue >= min_sig)))
+      tmp_dt = fread(.get_template(np_file), col.names = c("peak_width", cn))
+      out_dt = copy(tmp_dt)
+      out_dt$peak_width = peak_width
+      fwrite(out_dt, ofile, sep = " ")
+      out_dt$signal_cutoff = min_sig
+      dt = out_dt[, c("signal_cutoff", "peak_width", cn), with = FALSE]
+    }
+    width_res[[i]] = dt
+  }
+  out_dt = rbindlist(width_res)
+
+  out_dt[]
 }
